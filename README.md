3586ff5dbbcf63b951c8e644e385a4eac05975f7
yoshi <yoshinic@icloud.com>
2020-11-27 08:33:43 +0900

    新規

---
+++ README.md
@@ -0,0 +1,3 @@
+# COVID19Graph
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import COVID19GraphTests
+
+var tests = [XCTestCaseEntry]()
+tests += COVID19GraphTests.allTests()
+XCTMain(tests)
